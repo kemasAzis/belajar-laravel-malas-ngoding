7757e0ea4507034a4f5c63b1dda5c2f19446a1d8
kemas azis developer <nycekaziz@gmail.com>
2026-07-17 19:11:51 +0800

    perubahan kembali ke setelan awal

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DosenController;
-use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('halo', function () {
-    // return view('welcome');
-    return "hallo, malas ngoding ini adadalah route pertama saya";
-});
-
-Route::get('blog', function () {
-    return view('blog');
-});
-
-// memanggil mahasiswacontroller
-Route::get('mahasiswa', [MahasiswaController::class, 'index']);
-
-
-Route::get('dosen', [DosenController::class, 'index']);
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
-
-Route::get('/formulir', [PegawaiController::class, 'formulir']);
-Route::post('formulir/proses', [PegawaiController::class, 'proses']);
-
-// route blog
-Route::get('/blog', [BlogController::class, 'home']);
-Route::get('/blog/tentang', [BlogController::class, 'tentang']);
-Route::get('/blog/kontak', [BlogController::class, 'kontak']);
-
-
